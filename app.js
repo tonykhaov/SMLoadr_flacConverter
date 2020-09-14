48c93b9db57f5f0391b3f1f04500ab5cd17bd4df
Tony Khaov <tony.khaov@gmail.com>
2020-09-14 18:02:20 +0200

    Get all the artist folders inside DOWNLOADS

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const path = require("path");
 
 const DOWNLOADS_dir = path.join(__dirname, "/DOWNLOADS");
 
-console.log(DOWNLOADS_dir);
-
 fs.readdir(DOWNLOADS_dir, {}, (err, files) => {
   if (err) return console.log(err);
-  else return console.log(typeof files);
+  const artistFolders = files.filter((file) => file !== ".DS_Store");
+  console.log(artistFolders);
 });
