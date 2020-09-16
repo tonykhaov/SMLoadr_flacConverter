a68e064077c6c4e2a386ba1089f5f68d6eac8b14
Tony Khaov <tony.khaov@gmail.com>
2020-09-16 19:38:01 +0200

    Refactor the code to make it cleaner using async/await instead of callback hell

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs");
+const fsPromises = fs.promises;
 
-const DOWNLOADS_dir = path.join(__dirname, "/DOWNLOADS");
+const DOWNLOADS_DIR = path.join(__dirname, "/DOWNLOADS");
 
-fs.readdir(DOWNLOADS_dir, {}, (err, files) => {
-  if (err) return console.log(err);
-
-  const artistFolders = files.filter((file) => file !== ".DS_Store");
-  const artistFoldersPaths = artistFolders.map((artistName) =>
-    path.join(DOWNLOADS_dir, artistName)
-  );
-
-  artistFoldersPaths.forEach((artistFolder) => {
-    fs.readdir(artistFolder, {}, (err, files) => {
-      if (err) return console.log(err);
-
-      const albumFolders = files.filter((file) =>
-        /\(Album|Single\)$/.test(file)
-      );
-      const albumFoldersPaths = albumFolders.map((albumName) =>
-        path.join(artistFolder, albumName)
-      );
-
-      albumFoldersPaths.forEach((albumFolder) => {
-        fs.readdir(albumFolder, {}, (err, files) => {
-          if (err) console.log(err);
-
-          const songs = files.filter((file) => /\.flac$/.test(file));
-          const songsPaths = songs.map((song) => path.join(albumFolder, song));
-        });
-      });
-    });
+function getArtistsFolders(downloadsDir) {
+  return fsPromises.readdir(downloadsDir, {}, (err, files) => {
+    if (err) return err;
+    return files;
   });
-});
+}
+
+// artistFoldersPaths.forEach((artistFolder) => {
+//   fs.readdir(artistFolder, {}, (err, files) => {
+//     if (err) return console.log(err);
+
+//     const albumFolders = files.filter((file) => /\(Album|Single\)$/.test(file));
+//     const albumFoldersPaths = albumFolders.map((albumName) =>
+//       path.join(artistFolder, albumName)
+//     );
+
+//     albumFoldersPaths.forEach((albumFolder) => {
+//       fs.readdir(albumFolder, {}, (err, files) => {
+//         if (err) console.log(err);
+
+//         const songs = files.filter((file) => /\.flac$/.test(file));
+//         const songsPaths = songs.map((song) => path.join(albumFolder, song));
+//       });
+//     });
+//   });
+// });
+
+getArtistsFolders(DOWNLOADS_DIR).then((res) =>
+  console.log(res.map((r) => path.join(DOWNLOADS_DIR, r)))
+);
